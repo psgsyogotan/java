79dec15d9fd16b7ef12a7a2ec30f9e2d1b8e55de
p000526730 <shohgo.aizawa@nts.ricoh.co.jp>
2017-03-08 11:23:52 +0900

    add

--- src/jpl/ch17/ex01/GetAvailableMemory.java
+++ src/jpl/ch17/ex01/GetAvailableMemory.java
@@ -1,17 +1,18 @@
 package jpl.ch17.ex01;
 
+import java.util.ArrayList;
+
 public class GetAvailableMemory {
 
 	public static void main(String[] args) {
-		long[] Array = new long[50];
-		Runtime runtime = Runtime.getRuntime();
 
-		for (int i = 0; i < Array.length; i++) {
-			System.out.println(runtime.freeMemory());
-			Array[i] = i * 10000;
-			System.out.println(runtime.freeMemory());
-			System.out.println();
+		ArrayList<String> list = new ArrayList<String>();
 
+		for (int i = 0; i < 10000; i++) {
+			list.add("test"+i);
+			
+				System.out.println(Runtime.getRuntime().freeMemory());
+			
 		}
 	}
 
---
+++ src/jpl/ch17/ex02/DataHandler.java
@@ -0,0 +1,28 @@
+package jpl.ch17.ex02;
+
+import java.io.File;
+import java.lang.ref.WeakReference;
+
+public class DataHandler {
+	private WeakReference<File> lastFile;
+	private WeakReference<byte[]> lastData;
+
+	byte[] readFile(File file) {
+		byte[] data;
+
+		if (file.equals(lastFile.get())) {
+			data = lastData.get();
+			if (data != null)
+				return data;
+		}
+
+		//記憶していないので、読み込む
+		//readBytesFromFileの中身が不明
+		//自分で実装するということ？
+		data = readBytesFromFile(file);
+		lastData = new WeakReference<byte[]>(data);
+		return data;
+
+	}
+
+}
