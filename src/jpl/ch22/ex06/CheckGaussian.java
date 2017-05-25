af19ebfcd95ea70b32d2868725fa7ae28f795ff5
shohgo aizawa <shohgo.aizawa@nts.ricoh.co.jp>
2017-05-25 21:42:59 +0900

    add

--- src/jpl/ch22/ex06/CheckGaussian.java
+++ src/jpl/ch22/ex06/CheckGaussian.java
@@ -1,22 +1,43 @@
 package jpl.ch22.ex06;
 
+
+import java.util.Calendar;
 import java.util.Random;
 
 public class CheckGaussian {
 
-	public static void checkGaussian(int count) {
-		Random random = new Random();
-		int counter[] = new int[201];
+	public static void checkGaussian2(int count) {
+		Calendar calendar = Calendar.getInstance();
+		long seed = calendar.getTimeInMillis();
+		Random random = new Random(seed);
+		int counter[] = new int[10];
 
 		for (int i = 0; i < count; i++) {
 			double num = random.nextGaussian();
-			if (num >= -1 && num <= 1.0) {
-				counter[(int) (num * 100 + 100)]++;
-
+			if (num >= -1 && num < -0.8) {
+				counter[0]++;
+			} else if (num >= -0.8 && num < -0.6) {
+				counter[1]++;
+			} else if (num >= -0.6 && num < -0.4) {
+				counter[2]++;
+			} else if (num >= -0.4 && num < -0.2) {
+				counter[3]++;
+			} else if (num >= -0.2 && num < 0) {
+				counter[4]++;
+			} else if (num >= 0 && num < 0.2) {
+				counter[5]++;
+			} else if (num >= 0.2 && num < 0.4) {
+				counter[6]++;
+			} else if (num >= 0.4 && num < 0.6) {
+				counter[7]++;
+			} else if (num >= 0.6 && num < 0.8) {
+				counter[8]++;
+			} else if (num >= 0.8 && num <= 1.0) {
+				counter[9]++;
 			}
 		}
 		for (int i = 0; i < counter.length; i++) {
-			System.out.printf("%.2f"+": ",-1.000 + i * 0.01);
+			System.out.printf("%.2f" + "~: ", -1.000 + i * 0.2);
 			for (int j = 0; j < counter[i]; j++)
 				System.out.print("*");
 			System.out.println();
@@ -26,6 +47,6 @@ public class CheckGaussian {
 	}
 
 	public static void main(String[] args) {
-		CheckGaussian.checkGaussian(20000);
+		CheckGaussian.checkGaussian2(1000);
 	}
 }
