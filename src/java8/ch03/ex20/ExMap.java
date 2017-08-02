84afc937e84f8645c720c831aca51fa43874581a
Syogo <Syogo@Owner>
2017-08-03 07:55:42 +0900

    add

--- src/java8/ch03/ex20/ExMap.java
+++ src/java8/ch03/ex20/ExMap.java
@@ -1,5 +1,16 @@
 package java8.ch03.ex20;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
 public class ExMap {
+	public static <T, U> List<U> map(List<T> list, Function<T, U> function) {
+	    List<U> result = new ArrayList<U>();
+	    for (T element : list) {
+	        result.add(function.apply(element));
+	    }
+	    return result;
+	}
 
 }
--- src/java8/ch03/ex23/Ex23Map.java
+++ src/java8/ch03/ex23/Ex23Map.java
@@ -1,5 +1,20 @@
 package java8.ch03.ex23;
 
+import java.util.function.Function;
+
 public class Ex23Map {
+	public class Pair<T> {
+	    public final T first;
+	    public final T second;
+
+	    public Pair(T first, T second) {
+	        this.first = first;
+	        this.second = second;
+	    }
+
+	    public <U> Pair<U> map(Function<T, U> f) {
+	        return new Pair<U>(f.apply(first), f.apply(second));
+	    }
+	}
 
 }
